185c96c41519b29479fc24f323decee3274015ec
SphericalCowww <tinglin194@gmail.com>
2026-08-20 08:42:54 +0200

    - scanUpdateID_zeroing.cpp to scan occupied ID

--- src/my_toolbox_scs_workbench/src/testSpin_servo1.cpp
+++ src/my_toolbox_scs_workbench/src/testSpin_servo1.cpp
@@ -1,39 +1,39 @@
 #include <iostream>
 #include "SCServo.h"
-////////////////////////////////////////////////////////////
-SCSCL scs_wb;
-
-u8 ID[1] = {11};
-u16 Position[1];
-u16 Speed[1];
-////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 int main()
 {
     const char* port_name = "/dev/ttyACM0";
     int         baud_rate = 1000000;
-
-    if(!scs_wb.begin(baud_rate, port_name)){
-        std::cout<<"Failed to init scscl motor!"<<std::endl;
+ 
+    u8  ID [1] = {1};
+    s16 pos[1];
+    u16 vel[1];
+    u8  acc[1] = {0};
+    //////////////////////////////////////////////////////////////////////////
+    SMS_STS sts_wb;
+    if(!sts_wb.begin(baud_rate, port_name)){
+        std::cout<<"testSpin_servo1(): failed to initialize SMS_STS servo!"<<std::endl;
         return 0;
     }
 	while(1){
-		Position[0] = 1000;
-		Speed[0] = 1500;                // unit of 0.0146rpm
-		scs_wb.SyncWritePos(ID, 1, Position, 0, Speed);
+		pos[0] = 1000;
+		vel[0] = 1500;                                          // unit of 0.0146rpm
+		sts_wb.SyncWritePosEx(ID, 1, pos, vel, acc);
 		std::cout<<"pos = "<<1000<<std::endl;
-		usleep(754*1000);//[(P1-P0)/V]*1000+100
+		usleep(1000*1000);
   
-		Position[0] = 20;
-		Speed[0] = 1500;
-		scs_wb.SyncWritePos(ID, 1, Position, 0, Speed);
+		pos[0] = 20;
+		vel[0] = 1500;
+		sts_wb.SyncWritePosEx(ID, 1, pos, vel, acc);
 		std::cout<<"pos = "<<20<<std::endl;
-		usleep(754*1000);//[(P1-P0)/V]*1000+100
+		usleep(1000*1000);
 	}
-	scs_wb.end();
+	sts_wb.end();
 	return 1;
 }
-////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
